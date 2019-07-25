f839fc3f48c4e020fbbab65d9f4f5f9736de3d60
Kool-Aid-Ken <52799414+Kool-Aid-Ken@users.noreply.github.com>
2019-07-25 12:29:51 -0700

    Add files via upload

--- terms_page.js
+++ terms_page.js
@@ -12,88 +12,119 @@ var DATA = [{
     "synonyms": []
   },
   {
-    "name": Terms "description":
-    Git - Git is a distributed version-control system for tracking changes in source code during software development.
-    Version Control - The management of changes to documents, computer programs, large web sites, and other collections of information.
-    Centralized Model - A Centralized Training Organization is one that organizes the training function into a single or central group within the enterprise.
+    "name": Terms "description": Git - Git is a distributed version - control system
+    for tracking changes in source code during software development.
+    Version Control - The management of changes to documents,
+    computer programs,
+    large web sites,
+    and other collections of information.
+    Centralized Model - A Centralized Training Organization is one that organizes the training
+    function into a single or central group within the enterprise.
     Distributed Model - Defines a way of contact in between the components of a system and it refers to how resources are spread out and works on more than one device to improve the effectiveness and performance of a task.
     Zip Files - ZIP is an archive file format that supports lossless data compression.
-    Binary - relating to, composed of, or involving two things.
+    Binary - relating to,
+    composed of ,
+    or involving two things.
     Hexadecimal - Hex or base 16 or hexadecimal is a numeral system that uses 16 symbols.
-    Bits - a small piece, part, or quantity of something.
-    Nibble - In computing, a nibble is a four-bit aggregation, or half an octet. It is also known as half-byte or tetrade.
-    Byte - The byte is a unit of digital information that most commonly consists of eight bits, representing a binary number.
-    Kilo - The kilogram is the base unit of mass in the metric system, formally the International System of Units, having the unit symbol kg.
-    Mega - very large; huge.
+    Bits - a small piece,
+    part,
+    or quantity of something.
+    Nibble - In computing,
+    a nibble is a four - bit aggregation,
+    or half an octet.It is also known as half - byte or tetrade.
+    Byte - The byte is a unit of digital information that most commonly consists of eight bits,
+    representing a binary number.
+    Kilo - The kilogram is the base unit of mass in the metric system,
+    formally the International System of Units,
+    having the unit symbol kg.
+    Mega - very large;huge.
     Giga - Giga is a unit prefix in the metric system denoting a factor of a billion.
     Tera - denoting a factor of 1012.
     AND && (BASH)
     OR || (BASH)
-    Function(BASH) - The command list between curly braces {} is the body of the function.
-    Variable(BASH) - not consistent or having a fixed pattern; liable to change.
-    String(BASH) - In computer programming, a string is traditionally a sequence of characters, either as a literal constant or as some kind of variable. 
-    Desktop
-    Menu Bar
-    Dock
-    Launchpad
-    Window
-    Applications
-    Web Browser
-    Terminal
-    Bash
-    Finder[Finding / Organizing]
-    Files
-    Folders
-    File System / File Hierarchy
-    Compressed Files[Zips]
-    Bytes
-    Relative Paths
-    Absolute Paths
-    Computer System
-    Hardware
-    Software
-    Peripheral(Internal & External) Hardware
-    Process Cycle
-    Input & Output(I / O)
-    Case
-    Motherboard & The Bus
-    Ports
-    Processor(CPU)
-    Main Memory(RAM)
-    Secondary Memory(Hard Drive)
-    BIOS(ROM)
-    System Software
-    Operating System
-    Application Software
-    Programs
-    Computational Thinking
-    Programming
-    Decomposition
-    Pattern Recognition
-    Abstraction
-    Algorithm
-    PseudoCode
-    Flow Charts
-    Sequence,
-    Selection & Repetition
-    Evaluation
-    Debugging
-    Computer Scientist
-    Freelance
-    IT
-    Data Analyst
-    Computer Network Architect
-    Computer Support Specialist
-    Computer System Analyst
-    Computer System Administrator
-    Computer Science
-    Technology
-    Computers
-    Digital Divide
-    Problem Solving
-    Logical Thinking
-    Creative Thinking
-    Critical Thinking
+    Function(BASH) - The command list between curly braces {}
+    is the body of the
+    function.
+    Variable(BASH) - not consistent or having a fixed pattern;liable to change.
+    String(BASH) - In computer programming,
+    a string is traditionally a sequence of characters,
+    either as a literal constant or as some kind of variable.
+    Desktop - the working area of a computer screen regarded as a representation of a notional desktop and containing icons representing items such as files and a wastebasket.
+    Menu Bar - A menu bar is a graphical control element which contains drop - down menus.
+    Dock - a device in which a laptop computer,
+    smartphone,
+    or other mobile device may be placed
+    for charging,
+    providing access to a power supply and to peripheral devices or auxiliary features;a docking station.
+    Launchpad - Launchpad is a web application and website that allows users to develop and maintain software,
+    particularly open - source software.
+    Window - A window is a separate viewing area on a computer display screen in a system that allows multiple viewing areas as part of a graphical user interface(GUI).
+    Applications - An application is any program,
+    or group of programs,
+    that is designed
+    for the end user.
+    Web Browser - A web browser,
+    or simply "browser,"
+    is an application used to access and view websites.
+    Terminal - A computer terminal is an electronic or electromechanical hardware device that is used
+    for entering data into,
+    and displaying or printing data from,
+    a computer or a computing system.
+    Bash - It is a command language interpreter derived from sh that can execute commands entered at a command prompt and process text file input.
+    Finder[Finding / Organizing] - The Finder is the
+    default file manager and graphical user interface shell used on all Macintosh operating systems.
+    Files - a folder or box for holding loose papers that are typically arranged in a particular order for easy reference.
+    Folders - When talking about file systems, a folder (also called directory, or catalog) is a way to organize computer files.
+    File System / File Hierarchy - A filesystem is the methods and data structures that an operating system uses to keep track of files on a disk or partition;
+    Compressed Files[Zips] - ZIP is an archive file format that supports lossless data compression.
+    Relative Paths - A relative path is a way to specify the location of a directory relative to another directory.
+    Absolute Paths - An absolute path refers to the complete details needed to locate a file or folder, starting from the root element and ending with the other subdirectories.
+    Computer System - A system of interconnected computers that share a central storage system and various peripheral devices such as a printers, scanners, or routers.
+    Hardware - Computer hardware is the collection of physical parts of a computer system.
+    Software - Computer software, or simply software, is a collection of data or computer instructions that tell the computer how to work.
+    Peripheral(Internal & External) Hardware - A peripheral or peripheral device is an ancillary device used to put information into and get information out of the computer.
+    Process Cycle - Process Cycle Efficiency is calculated by dividing the value-added time associated with a process by the total lead time of the process.
+    Input & Output(I / O) - In computing, input/output or I/O is the communication between an information processing system, such as a computer, and the outside world, possibly a human or another information processing system.
+    Case - A computer case, also known as a computer chassis, tower, system unit, CPU (when referring to the case as a whole rather than the processor), or cabinet, is the enclosure that contains most of the components of a personal computer (
+    Motherboard & The Bus - a printed circuit board containing the principal components of a computer or other device, with connectors into which other circuit boards can be slotted.
+    Ports - Internally, there are several ports for connecting disk drives, display screens, and keyboards.
+    Processor(CPU) - The CPU contains at least one processor, which is the actual chip inside the CPU that performs calculations.
+    Main Memory(RAM) - Random-access memory is a form of computer data storage that can be read and changed in any order, typically used to store working data and machine code.
+    Secondary Memory(Hard Drive) - A hard disk drive (sometimes abbreviated as hard drive, HD, or HDD) is a non-volatile memory hardware device that permanently stores and retrieves data on a computer.
+    BIOS(ROM) - BIOS is non-volatile firmware used to perform hardware initialization during the booting process, and to provide runtime services for operating systems and programs.
+    System Software - System software is software designed to provide a platform for other software.
+    Operating System - An operating system is system software that manages computer hardware and software resources and provides common services for computer programs.
+    Application Software - An application is any program, or group of programs, that is designed for the end user.
+    Programs - A computer program is a collection of instructions that performs a specific task when executed by a computer.
+    Computational Thinking - In education, computational thinking (CT) is a set of problem-solving methods that involve expressing problems and their solutions in ways that a computer could execute.
+    Programming - Programming is the process of taking an algorithm and encoding it into a notation, a programming language, so that it can be executed by a computer.
+    Decomposition - Decomposition in computer science, also known as factoring, is breaking a complex problem or system into parts that are easier to conceive
+    Pattern Recognition - Pattern recognition is the process of recognizing patterns by using machine learning algorithm.
+    Abstraction - In software engineering and computer science, abstraction is: the process of removing physical, spatial, or temporal details or attributes in the study of objects or systems in order to focus attention
+    Algorithm - In mathematics and computer science, an algorithm is a set of instructions, typically to solve a class of problems or perform a computation.
+    PseudoCode - Pseudocode is an informal high-level description of the operating principle of a computer program or other algorithm.
+    Flow Charts - A flowchart is a type of diagram that represents a workflow or process.
+    Sequence - In mathematics, a sequence is an enumerated collection of objects in which repetitions are allowed.
+    Selection - the action or fact of carefully choosing someone or something as being the best or most suitable.
+    Repetition - Repetition is the simple repeating of a word, within a short space of words, with no particular placement of the words to secure emphasis.
+    Evaluation - the making of a judgment about the amount, number, or value of something; assessment.
+    Debugging - the process of identifying and removing errors from computer hardware or software.
+    Computer Scientist - A computer scientist is a person who has acquired the knowledge of computer science.
+    Freelance - Freelance, freelancer, and freelance worker, are terms commonly used for a person who is self-employed and is not necessarily committed to a particular employer long-term.
+    IT - Information technology is the use of computers to store, retrieve, transmit, and manipulate data, or information, often in the context of a business or other enterprise.
+    Data Analyst - Data analysts translate numbers into plain English Every business collects data, whether it's sales figures, market research, logistics, or transportation costs.
+    Computer Network Architect - Computer network architects design and build data communication networks, including local area networks (LANs), wide area networks (WANs), and intranets.
+    Computer Support Specialist - They help IT staff analyze, troubleshoot, and evaluate computer network problems. ... Technical support specialists may provide assistance to the organization's computer users through phone, email, or in-person visits.
+    Computer System Analyst - A systems analyst is a person who uses analysis and design techniques to solve business problems using information technology.
+    Computer System Administrator - Network and computer systems administrators are responsible for the day-to-day operation of these networks.
+    Computer Science - Computer Science is the study of computers and computational systems.
+    Technology - Technology ("science of craft", from Greek τέχνη, techne, "art, skill, cunning of hand"; and -λογία, -logia) is the collection of techniques, skills, methods, and processes used in the production of goods or services or in the accomplishment of objectives, such as scientific investigation.
+    Computers - an electronic device for storing and processing data, typically in binary form, according to instructions given to it in a variable program.
+    Digital Divide - A digital divide is any uneven distribution in the access to, use of, or impact of information and communication technologies between any number of distinct groups.
+    Problem Solving - Problem solving consists of using generic or ad hoc methods in an orderly manner to find solutions to problems.
+    Logical Thinking - Logical thinking is the process in which one uses reasoning consistently to come to a conclusion.
+    Creative Thinking -  A way of looking at problems or situations from a fresh perspective that suggests unorthodox solutions
+    Critical Thinking - Critical thinking is the analysis of facts to form a judgment.
 
 
 
